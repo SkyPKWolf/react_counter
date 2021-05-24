361b5a6ecf1341f1c53a172b3928f504719bdd9b
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-05-24 12:45:23 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.5a41f9e582cb6d5ec5507cb22c048ae1.js"
 );
 
 self.addEventListener('message', (event) => {
